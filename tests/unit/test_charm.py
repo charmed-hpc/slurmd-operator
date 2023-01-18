d0a2ae62061a81083c13ace4f6d153f6a2520a26
David Gomez <david.gomez@canonical.com>
2023-01-18 14:19:59 -0800

    Enhance: feedback update

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -7,13 +7,14 @@ from ops.testing import Harness
 
 
 class TestCharm(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
         self.harness = Harness(SlurmdCharm)
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
     @patch("ops.framework.EventBase.defer")
     def test_check_etcd_fail(self, defer):
+        """Test check_etcd method failure behavior."""
         self.harness.charm.on.check_etcd.emit()
         defer.assert_called()
 
@@ -25,22 +26,26 @@ class TestCharm(unittest.TestCase):
     @patch("charm.SlurmdCharm.etcd_use_tls", new_callable=PropertyMock(return_value=False))
     @patch("ops.framework.EventBase.defer")
     def test_check_etcd_success(self, defer, *_):
+        """Test check_etcd method success behavior."""
         self.harness.charm.on.check_etcd.emit()
         defer.assert_not_called()
 
     @patch("ops.framework.EventBase.defer")
     def test_config_changed_fail(self, defer):
+        """Test config_changed failure behavior."""
         self.harness.set_leader(True)
         self.harness.charm.on.config_changed.emit()
         defer.assert_called()
 
     @patch("ops.framework.EventBase.defer")
     def test_config_changed_success(self, defer):
+        """Test config_changed success behavior."""
         self.harness.charm.on.config_changed.emit()
         defer.assert_not_called()
 
     @patch("ops.framework.EventBase.defer")
     def test_install_fail(self, defer):
+        """Test install failure behavior."""
         self.harness.charm.on.install.emit()
         self.assertFalse(self.harness.charm._stored.slurm_installed)
         defer.assert_called()
@@ -51,16 +56,19 @@ class TestCharm(unittest.TestCase):
     @patch("ops.model.Resources.fetch")
     @patch("ops.framework.EventBase.defer")
     def test_install_success(self, defer, *_):
+        """Test install success behavior."""
         self.harness.charm.on.install.emit()
         self.assertTrue(self.harness.charm._stored.slurm_installed)
         defer.assert_not_called()
 
     def test_slurmctld_started(self):
+        """Test slurmctld_started works."""
         self.harness.charm.on.slurmctld_started.emit()
         self.assertTrue(self.harness.charm._stored.slurmctld_started)
 
     @patch("ops.framework.EventBase.defer")
     def test_slurmd_start_fail(self, defer):
+        """Test slurmd_start failure behavior."""
         self.harness.charm.on.slurmd_start.emit()
         defer.assert_called()
 
@@ -69,11 +77,12 @@ class TestCharm(unittest.TestCase):
         new_callable=PropertyMock(return_value=False),
     )
     @patch("interface_slurmd.Slurmd.is_joined", new_callable=PropertyMock(return_value=True))
-    @patch("slurm_ops_manager.SlurmManager.check_munged", lambda _: True)
-    @patch("slurm_ops_manager.SlurmManager.slurm_is_active", lambda _: True)
+    @patch("slurm_ops_manager.SlurmManager.check_munged", return_value=True)
+    @patch("slurm_ops_manager.SlurmManager.slurm_is_active", return_value=True)
     @patch("slurm_ops_manager.SlurmManager.slurm_systemctl", lambda *_: "stop", True)
     @patch("ops.framework.EventBase.defer")
     def test_slurmd_start_success(self, defer, *_):
+        """Test slurmd_start success behavior."""
         self.harness.charm._stored.slurm_installed = True
         self.harness.charm._stored.slurmctld_available = True
         self.harness.charm._stored.slurmctld_started = True
@@ -87,6 +96,7 @@ class TestCharm(unittest.TestCase):
         new_callable=PropertyMock(return_value=False),
     )
     def test_update_status_install_fail(self, _):
+        """Test update_status failure behavior from install."""
         self.harness.charm.on.update_status.emit()
         self.assertEqual(self.harness.charm.unit.status, BlockedStatus("Error installing slurmd"))
 
@@ -94,6 +104,7 @@ class TestCharm(unittest.TestCase):
         "slurm_ops_manager.SlurmManager.needs_reboot", new_callable=PropertyMock(return_value=True)
     )
     def test_update_status_needs_reboot(self, _):
+        """Test update_status failure behavior from reboot."""
         self.harness.charm.on.update_status.emit()
         self.assertEqual(self.harness.charm.unit.status, BlockedStatus("Machine needs reboot"))
 
@@ -102,8 +113,9 @@ class TestCharm(unittest.TestCase):
         new_callable=PropertyMock(return_value=False),
     )
     @patch("interface_slurmd.Slurmd.is_joined", new_callable=PropertyMock(return_value=True))
-    @patch("slurm_ops_manager.SlurmManager.check_munged", lambda _: True)
+    @patch("slurm_ops_manager.SlurmManager.check_munged", return_value=True)
     def test_update_status_success(self, *_):
+        """Test update_status success behavior."""
         self.harness.charm._stored.slurm_installed = True
         self.harness.charm._stored.slurmctld_available = True
         self.harness.charm._stored.slurmctld_started = True
