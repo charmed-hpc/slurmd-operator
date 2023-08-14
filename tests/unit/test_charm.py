e08877d0695055f2ab6f6ac7d513988efea9e3a7
NucciTheBoss <jason.nucciarone@canonical.com>
2023-08-14 16:31:32 -0400

    tests: Update unit tests
    Signed-off-by: Jason C. Nucciarone <jason.nucciarone@canonical.com>

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -13,13 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Test default charm events such as upgrade charm, install, etc."""
+"""Unit tests for the slurmd operator."""
 
 import unittest
 from unittest.mock import PropertyMock, patch
 
 from charm import SlurmdCharm
-from ops.model import ActiveStatus, BlockedStatus
+from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus
 from ops.testing import Harness
 
 
@@ -31,14 +31,6 @@ class TestCharm(unittest.TestCase):
         self.harness = Harness(SlurmdCharm)
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
-
-    @patch("charm.SlurmdCharm._on_slurmctld_started")
-    @patch("charm.SlurmdCharm.hostname", new_callable=PropertyMock(return_value="test"))
-    @patch("ops.framework.EventBase.defer")
-    def test_check_etcd_success(self, defer, *_) -> None:
-        """Test check_etcd method success behavior."""
-        self.harness.charm.on.check_etcd.emit()
-        defer.assert_not_called()
 
     @patch("ops.framework.EventBase.defer")
     def test_config_changed_fail(self, defer) -> None:
@@ -64,6 +56,9 @@ class TestCharm(unittest.TestCase):
     @patch("pathlib.Path.read_text", return_value="v1.0.0")
     @patch("ops.model.Unit.set_workload_version")
     @patch("ops.model.Resources.fetch")
+    @patch("utils.slurmd.override_default")
+    @patch("utils.slurmd.override_service")
+    @patch("charms.hpc_libs.v0.juju_systemd_notices.SystemdNotices.subscribe")
     @patch("ops.framework.EventBase.defer")
     def test_install_success(self, defer, *_) -> None:
         """Test install success behavior."""
@@ -71,31 +66,15 @@ class TestCharm(unittest.TestCase):
         self.assertTrue(self.harness.charm._stored.slurm_installed)
         defer.assert_not_called()
 
-    def test_slurmctld_started(self) -> None:
-        """Test slurmctld_started works."""
-        self.harness.charm.on.slurmctld_started.emit()
-        self.assertTrue(self.harness.charm._stored.slurmctld_started)
+    def test_service_slurmd_start(self) -> None:
+        """Test service_slurmd_started event handler."""
+        self.harness.charm.on.service_slurmd_started.emit()
+        self.assertEqual(self.harness.charm.unit.status, ActiveStatus())
 
-    @patch("ops.framework.EventBase.defer")
-    def test_slurmd_start_fail(self, defer) -> None:
-        """Test slurmd_start failure behavior."""
-        self.harness.charm.on.slurmd_start.emit()
-        defer.assert_called()
-
-    @patch("interface_slurmd.Slurmd.is_joined", new_callable=PropertyMock(return_value=True))
-    @patch("slurm_ops_manager.SlurmManager.check_munged", return_value=True)
-    @patch("slurm_ops_manager.SlurmManager.slurm_is_active", return_value=True)
-    @patch("slurm_ops_manager.SlurmManager.slurm_systemctl", lambda *_: "stop", True)
-    @patch("ops.framework.EventBase.defer")
-    def test_slurmd_start_success(self, defer, *_) -> None:
-        """Test slurmd_start success behavior."""
-        self.harness.charm._stored.slurm_installed = True
-        self.harness.charm._stored.slurmctld_available = True
-        self.harness.charm._stored.slurmctld_started = True
-
-        self.harness.charm.on.slurmd_start.emit()
-        self.assertEqual(self.harness.charm.unit.status, ActiveStatus("slurmd available"))
-        defer.assert_not_called()
+    def test_service_slurmd_stopped(self) -> None:
+        """Test service_slurmd_stopped event handler."""
+        self.harness.charm.on.service_slurmd_stopped.emit()
+        self.assertEqual(self.harness.charm.unit.status, BlockedStatus("slurmd off"))
 
     def test_update_status_install_fail(self) -> None:
         """Test update_status failure behavior from install."""
@@ -111,4 +90,5 @@ class TestCharm(unittest.TestCase):
         self.harness.charm._stored.slurmctld_started = True
 
         self.harness.charm.on.update_status.emit()
-        self.assertEqual(self.harness.charm.unit.status, ActiveStatus("slurmd available"))
+        # Status should not change at all as slurmd should be active.
+        self.assertEqual(self.harness.charm.unit.status, MaintenanceStatus())
