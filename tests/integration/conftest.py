f8a96f6d83f4e49104917928453f95825f6ce377
NucciTheBoss <jason.nucciarone@canonical.com>
2023-08-24 13:15:14 -0400

    tests: Add support for using local charms in integration tests
    * Added `--use-local` test option to direct pytest to use local charms.
        Invoked with `tox run -e integration -- --use-local`
    * pytest fixtures for charms return a generic string if a local
        copy of the charm does not exist. Returns a Path if a charm
        is packed.
    * Made `get_slurmd_res()` a coroutine so it could be used with asyncio.gather(...)
    * Updated pylibjuju to generic version rather than pin to 3.1.0.1.
    * Removed pylxd from integration test dependencies. We are no longer using
        it because we are no longer modifying the default LXD profile before deploying operators.
    
    Signed-off-by: Jason C. Nucciarone <jason.nucciarone@canonical.com>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -13,31 +13,87 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Configure integration test run."""
+"""Configure slurmd operator integration tests."""
 
+import logging
+import os
 from pathlib import Path
+from typing import Union
 
 import pytest
 from helpers import NHC
 from pytest_operator.plugin import OpsTest
 
+logger = logging.getLogger(__name__)
+SLURMCTLD_DIR = Path(os.getenv("SLURMCTLD_DIR", "../slurmctld-operator"))
+SLURMDBD_DIR = Path(os.getenv("SLURMDBD_DIR", "../slurmdbd-operator"))
+
 
 def pytest_addoption(parser) -> None:
     parser.addoption(
-        "--charm-base", action="store", default="ubuntu@22.04", help="Charm base to test."
+        "--charm-base",
+        action="store",
+        default="ubuntu@22.04",
+        help="Charm base version to use for integration tests.",
+    )
+    parser.addoption(
+        "--use-local",
+        action="store_true",
+        default=False,
+        help="Use SLURM operators located on localhost rather than pull from Charmhub",
     )
 
 
 @pytest.fixture(scope="module")
 def charm_base(request) -> str:
     """Get slurmd charm base to use."""
-    return request.config.getoption("--charm-base")
+    return request.config.option.charm_base
 
 
 @pytest.fixture(scope="module")
-async def slurmd_charm(ops_test: OpsTest):
-    """Build slurmd charm to use for integration tests."""
+async def slurmd_charm(ops_test: OpsTest) -> Path:
+    """Pack slurmd charm to use for integration tests."""
     return await ops_test.build_charm(".")
+
+
+@pytest.fixture(scope="module")
+async def slurmctld_charm(request, ops_test: OpsTest) -> Union[str, Path]:
+    """Pack slurmctld charm to use for integration tests when --use-local is specified.
+
+    Returns:
+        `str` "slurmctld" if --use-local not specified or if SLURMD_DIR does not exist.
+    """
+    if request.config.option.use_local:
+        logger.info("Using local slurmctld operator rather than pulling from Charmhub")
+        if SLURMCTLD_DIR.exists():
+            return await ops_test.build_charm(SLURMCTLD_DIR)
+        else:
+            logger.warning(
+                f"{SLURMCTLD_DIR} not found. "
+                f"Defaulting to latest/edge slurmctld operator from Charmhub"
+            )
+
+    return "slurmctld"
+
+
+@pytest.fixture(scope="module")
+async def slurmdbd_charm(request, ops_test: OpsTest) -> Union[str, Path]:
+    """Pack slurmdbd charm to use for integration tests when --use-local is specified.
+
+    Returns:
+        `str` "slurmdbd" if --use-local not specified or if SLURMDBD_DIR does not exist.
+    """
+    if request.config.option.use_local:
+        logger.info("Using local slurmdbd operator rather than pulling from Charmhub")
+        if SLURMDBD_DIR.exists():
+            return await ops_test.build_charm(SLURMDBD_DIR)
+        else:
+            logger.warning(
+                f"{SLURMDBD_DIR} not found. "
+                f"Defaulting to latest/edge slurmdbd operator from Charmhub"
+            )
+
+    return "slurmdbd"
 
 
 def pytest_sessionfinish(session, exitstatus) -> None:
